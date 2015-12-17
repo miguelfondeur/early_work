89e7898982befac9266880b3a090c207083534f6
miguelfondeur <miguel@solveitsimply.com>
2015-12-17 03:57:13 -0500

    add propper text to shes the first and malltip

--- routes/shestf.js
+++ routes/shestf.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shestf', { title: 'Shes The First' });
+  res.render('shestf', { title: 'She\'s The First' });
 });
 
 module.exports = router;
